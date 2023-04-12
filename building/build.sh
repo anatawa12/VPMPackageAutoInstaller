9c42fe61a19d0a2ef40e963453653d71a1a87b36
anatawa12 <anatawa12@icloud.com>
2023-04-13 01:13:34 +0900

    chore: move script to build

---
+++ building/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eu
+
+cd "$(dirname "$0")"
+BUID_DIR="$(pwd)"
+cd ".."
+REPO_ROOT="$(pwd)"
+INSTALLER_DIR="$REPO_ROOT/Assets/com.anatawa12.vpm-package-auto-installer"
+
+build_dll() {
+  DOTNET_DIR="$REPO_ROOT/Assets/InstallerSource/DllBuild~"
+  PROFILE="Release"
+  # build
+  dotnet build "$DOTNET_DIR/com.anatawa12.vpm-package-auto-installer.csproj" -c "$PROFILE" 
+  
+  # copy dll and meta
+  cp "$DOTNET_DIR/bin/$PROFILE/netstandard2.0/com.anatawa12.vpm-package-auto-installer.dll" "$INSTALLER_DIR/"
+  cp "$BUID_DIR/com.anatawa12.vpm-package-auto-installer.dll.meta" "$INSTALLER_DIR/"
+}
+
+build() {
+  build_dll
+}
+
+if [ $# -eq 0 ]; then
+  build
+else
+  # you can call specific build process by parameter
+  "$@"
+fi
