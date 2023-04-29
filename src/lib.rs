c168fe9895af42dd974b780b7940273cdde7c3fe
anatawa12 <anatawa12@icloud.com>
2023-04-29 21:25:37 +0900

    feat: add entrypoint for wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,45 @@
 use std::io::{Cursor, Read, Result, Write};
 
+#[cfg(target_arch = "wasm32")]
+pub extern "C" fn create_unitypackage_wasm(
+    template_ptr: *const u8, 
+    template_len: usize, 
+    json_ptr: *const u8,
+    json_len: usize,
+) -> *const () {
+    #[allow(dead_code)]
+    struct Result {
+        ptr: *const u8,
+        len: usize,
+        is_err: bool,
+    }
+
+    let template = unsafe { std::slice::from_raw_parts(template_ptr, template_len) };
+    let json = unsafe { std::slice::from_raw_parts(json_ptr, json_len) };
+    let mut out_buf = Vec::<u8>::new();
+
+    let result: (Vec<u8>, bool);
+    match create_unitypackage(template, &mut out_buf, json) {
+        Ok(()) => {
+            // ok: return buffer
+            result = (out_buf, false);
+        }
+        Err(err) => {
+            result = (err.to_string().into_bytes(), false);
+        }
+    }
+
+    let leak = result.0.leak();
+
+    let boxed = Box::new(Result {
+        ptr: leak.as_ptr(),
+        len: leak.len(),
+        is_err: result.1,
+    });
+
+    Box::leak(boxed) as *const Result as _
+}
+
 pub fn create_unitypackage(template: &[u8], output: impl Write, json: &[u8]) -> Result<()> {
     let decoder = flate2::read::GzDecoder::new(Cursor::new(template));
     let encoder = flate2::write::GzEncoder::new(output, flate2::Compression::default());
